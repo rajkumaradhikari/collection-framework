b84dc824884f772058dfc4a18ddcaaadb70a91a2
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2024-08-08 00:26:07 +0545

    linked list using queue test

---
+++ src/LinedListQueueJava.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LinedListQueueJava {
+    public static void main(String[] args){
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+        queue.offer(40);
+
+        System.out.println(queue);
+        System.out.println(queue.poll());
+        System.out.println(queue);
+        System.out.println(queue.peek());
+
+
+    }
+}
